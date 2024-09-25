f703e32d5e6278a5f5f1dfc68935056fad43cbf3
cemdatade <cem.data.de@gmail.com>
2024-09-25 14:50:00 +0200

    '5.hafta ödev'

--- amazon_review_ödev.py
+++ amazon_review_ödev.py
@@ -75,19 +75,13 @@ df['helpful_yes'].nunique()
 df['helpful'].value_counts()
 df.describe()
 
-df_gecici=df[['reviewerID','asin', 'helpful',  'overall', 'reviewTime', 'day_diff', 'helpful_yes', 'total_vote']]
+df=df[['overall', 'reviewTime', 'day_diff', 'helpful_yes', 'total_vote']]
 
 
 df["asin"].nunique()
 
 df.groupby("asin").agg({"overall": ["mean"]})
 
-veya
-df_asin = df[df["asin"] == "B007WTAJTO"]
-df_asin.head()
-average_rating = df_asin["overall"].mean()
-
-veya
 
 df["overall"].mean()
 
@@ -112,65 +106,35 @@ df["reviewTime"] = pd.to_datetime(df["reviewTime"]) # Timestamp tipini datetime'
 df_sorted = df.sort_values(by='reviewTime', ascending=False)
 df_sorted.head()
 
-current_date = pd.to_datetime('2014-12-09 0:0:0')   # Şimdiki zamanı belirle
-
-df["days"] = (current_date - df["reviewTime"]).dt.days  # Tarih farkını gün olarak hesapla
-
-df[df["days"] <= 30].count()   # 30 günden önceki kayıt sayısı
-
-df.loc[df["days"] <= 30, "overall"].mean()    # 30 günden önceki puanların ortalaması
-
-df.loc[(df["days"] > 30) & (df["days"] <= 90), "overall"].mean()  # 30 ile 90 gün arasındaki puanların ortalaması
-
-df.loc[(df["days"] > 90) & (df["days"] <= 180), "overall"].mean()
-
-df.loc[(df["days"] > 180), "overall"].mean()
+#current_date = pd.to_datetime('2014-12-09 0:0:0')   # Şimdiki zamanı belirle
+#df["days"] = (current_date - df["reviewTime"]).dt.days  # Tarih farkını gün olarak hesapla
 
 
-df.loc[df["days"] <= 30, "overall"].mean() * 28/100 + \
-    df.loc[(df["days"] > 30) & (df["days"] <= 90), "overall"].mean() * 26/100 + \
-    df.loc[(df["days"] > 90) & (df["days"] <= 180), "overall"].mean() * 24/100 + \
-    df.loc[(df["days"] > 180), "overall"].mean() * 22/100
+df[df["day_diff"] <= 30].count()   # 30 günden önceki kayıt sayısı
+
+df.loc[df["day_diff"] <= 30, "overall"].mean()    # 30 günden önceki puanların ortalaması
+
+df.loc[(df["day_diff"] > 30) & (df["day_diff"] <= 90), "overall"].mean()  # 30 ile 90 gün arasındaki puanların ortalaması
+
+df.loc[(df["day_diff"] > 90) & (df["day_diff"] <= 180), "overall"].mean()
+
+df.loc[(df["day_diff"] > 180), "overall"].mean()
+
+
+df.loc[df["day_diff"] <= 30, "overall"].mean() * 28/100 + \
+    df.loc[(df["day_diff"] > 30) & (df["day_diff"] <= 90), "overall"].mean() * 26/100 + \
+    df.loc[(df["day_diff"] > 90) & (df["day_diff"] <= 180), "overall"].mean() * 24/100 + \
+    df.loc[(df["day_diff"] > 180), "overall"].mean() * 22/100
 
 def time_based_weighted_average(dataframe, w1=28, w2=26, w3=24, w4=22):
-    return dataframe.loc[df["days"] <= 30, "overall"].mean() * w1 / 100 + \
-           dataframe.loc[(dataframe["days"] > 30) & (dataframe["days"] <= 90), "overall"].mean() * w2 / 100 + \
-           dataframe.loc[(dataframe["days"] > 90) & (dataframe["days"] <= 180), "overall"].mean() * w3 / 100 + \
-           dataframe.loc[(dataframe["days"] > 180), "overall"].mean() * w4 / 100
+    return dataframe.loc[df["day_diff"] <= 30, "overall"].mean() * w1 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > 30) & (dataframe["day_diff"] <= 90), "overall"].mean() * w2 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > 90) & (dataframe["day_diff"] <= 180), "overall"].mean() * w3 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > 180), "overall"].mean() * w4 / 100
 
 time_based_weighted_average(df)
 
-time_based_weighted_average(df, 30, 26, 22, 22)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+time_based_weighted_average(df, 30, 25, 23, 22)
 
 
 
@@ -408,3 +372,368 @@ print(
         ]
     ]
 )
+
+
+
+#############################derya hoca ile ##########
+
+
+
+###################################################
+# PROJE: Rating Product & Sorting Reviews in Amazon
+###################################################
+
+###################################################
+# İş Problemi
+###################################################
+
+# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
+# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
+# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
+# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
+# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
+# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
+
+###################################################
+# Veri Seti Hikayesi
+###################################################
+
+# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
+# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
+
+# Değişkenler:
+# reviewerID: Kullanıcı ID’si
+# asin: Ürün ID’si
+# reviewerName: Kullanıcı Adı
+# helpful: Faydalı değerlendirme derecesi
+# reviewText: Değerlendirme
+# overall: Ürün rating’i
+# summary: Değerlendirme özeti
+# unixReviewTime: Değerlendirme zamanı
+# reviewTime: Değerlendirme zamanı Raw
+# day_diff: Değerlendirmeden itibaren geçen gün sayısı
+# helpful_yes: Değerlendirmenin faydalı bulunma sayısı
+# total_vote: Değerlendirmeye verilen oy sayısı
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import scipy.stats as st
+
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', 10)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+###################################################
+# GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
+###################################################
+
+# Paylaşılan veri setinde kullanıcılar bir ürüne puanlar vermiş ve yorumlar yapmıştır.
+# Bu görevde amacımız verilen puanları tarihe göre ağırlıklandırarak değerlendirmek.
+# İlk ortalama puan ile elde edilecek tarihe göre ağırlıklı puanın karşılaştırılması gerekmektedir.
+
+
+##################################################################################################################
+# Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
+#################################################################################################################
+
+df = pd.read_csv("amazon_review.csv")
+df.head()
+df.info()
+df.nunique()
+df.isnull().sum()
+df=df[['overall', 'reviewTime', 'day_diff', 'helpful_yes', 'total_vote']]
+df["overall"].mean()
+
+
+df.head()
+
+##################################################################################################################
+# Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
+#################################################################################################################
+
+
+df.info()
+
+
+df['reviewTime'] = pd.to_datetime(df['reviewTime'], dayfirst=True)
+
+#df_sorted = df.sort_values(by='reviewTime', ascending=False)
+#df_sorted.head()
+#current_date = pd.to_datetime(str(df['reviewTime'].max()))
+# df["day_diff"] = (current_date - df['reviewTime']).dt.days
+
+#df.sort_values(by='day_diff', ascending=False).tail()
+
+
+#################
+
+
+df[df["day_diff"] <= 30].count()
+
+df.loc[df["day_diff"] <= 30, "overall"].mean()
+
+df.loc[(df["day_diff"] > 30) & (df["day_diff"] <= 90), "overall"].mean()
+
+df.loc[(df["day_diff"] > 90) & (df["day_diff"] <= 180), "overall"].mean()
+
+df.loc[(df["day_diff"] > 180), "overall"].mean()
+
+
+df.loc[df["day_diff"] <= 30, "overall"].mean() * 28/100 + \
+    df.loc[(df["day_diff"] > 30) & (df["day_diff"] <= 90), "overall"].mean() * 26/100 + \
+    df.loc[(df["day_diff"] > 90) & (df["day_diff"] <= 180), "overall"].mean() * 24/100 + \
+    df.loc[(df["day_diff"] > 180), "overall"].mean() * 22/100
+
+def time_based_weighted_average(dataframe, w1=28, w2=26, w3=24, w4=22):
+    return dataframe.loc[df["day_diff"] <= 30, "overall"].mean() * w1 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > 30) & (dataframe["day_diff"] <= 90), "overall"].mean() * w2 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > 90) & (dataframe["day_diff"] <= 180), "overall"].mean() * w3 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > 180), "overall"].mean() * w4 / 100
+
+time_based_weighted_average(df)
+
+time_based_weighted_average(df, 30, 25, 23, 22)
+
+
+
+
+
+##################
+
+
+df.loc[df["day_diff"] <= df["day_diff"].quantile(0.25), "overall"].mean()
+df.loc[(df["day_diff"] > df["day_diff"].quantile(0.25)) & (df["day_diff"] <= df["day_diff"].quantile(0.50)), "overall"].mean()
+df.loc[(df["day_diff"] > df["day_diff"].quantile(0.50)) & (df["day_diff"] <= df["day_diff"].quantile(0.75)), "overall"].mean()
+df.loc[(df["day_diff"] > df["day_diff"].quantile(0.75)), "overall"].mean()
+
+
+# zaman bazlı ortalama ağırlıkların belirlenmesi
+def time_based_weighted_average(dataframe, w1=28, w2=26, w3=24, w4=22):
+    return dataframe.loc[dataframe["day_diff"] <= dataframe["day_diff"].quantile(0.25), "overall"].mean() * w1 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.25)) & (dataframe["day_diff"] <= dataframe["day_diff"].quantile(0.50)), "overall"].mean() * w2 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.50)) & (dataframe["day_diff"] <= dataframe["day_diff"].quantile(0.75)), "overall"].mean() * w3 / 100 + \
+           dataframe.loc[(dataframe["day_diff"] > dataframe["day_diff"].quantile(0.75)), "overall"].mean() * w4 / 100
+
+
+df.loc[df["day_diff"] <= df["day_diff"].quantile(0.25), "overall"].mean() * 28 / 100 + \
+df.loc[(df["day_diff"] > df["day_diff"].quantile(0.25)) & (df["day_diff"] <= df["day_diff"].quantile(0.50)), "overall"].mean() * 26 / 100 + \
+df.loc[(df["day_diff"] > df["day_diff"].quantile(0.50)) & (df["day_diff"] <= df["day_diff"].quantile(0.75)), "overall"].mean() * 24 / 100 + \
+df.loc[(df["day_diff"] > df["day_diff"].quantile(0.75)), "overall"].mean() * 22 / 100
+
+time_based_weighted_average(df, w1=28, w2=26, w3=24, w4=22)
+
+df["overall"].mean()
+
+
+###################################################
+# Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
+###################################################
+
+
+###################################################
+# Adım 1. helpful_no Değişkenini Üretiniz
+###################################################
+
+# Not:
+# total_vote bir yoruma verilen toplam up-down sayısıdır.
+# up, helpful demektir.
+# veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
+
+df.head()
+df.tail()
+
+df["helpful_no"] = df["total_vote"] - df["helpful_yes"]
+
+df = df[["reviewerName", "overall", "summary", "helpful_yes", "helpful_no", "total_vote", "reviewTime"]]
+
+df.head()
+
+###################################################
+# Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
+###################################################
+
+def wilson_lower_bound(up, down, confidence=0.95):
+    """
+    Wilson Lower Bound Score hesapla
+
+    - Bernoulli parametresi p için hesaplanacak güven aralığının alt sınırı WLB skoru olarak kabul edilir.
+    - Hesaplanacak skor ürün sıralaması için kullanılır.
+    - Not:
+    Eğer skorlar 1-5 arasıdaysa 1-3 negatif, 4-5 pozitif olarak işaretlenir ve bernoulli'ye uygun hale getirilebilir.
+    Bu beraberinde bazı problemleri de getirir. Bu sebeple bayesian average rating yapmak gerekir.
+
+    Parameters
+    ----------
+    up: int
+        up count
+    down: int
+        down count
+    confidence: float
+        confidence
+
+    Returns
+    -------
+    wilson score: float
+
+    """
+    n = up + down
+    if n == 0:
+        return 0
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    phat = 1.0 * up / n
+    return (phat + z * z / (2 * n) - z * math.sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+
+
+def score_up_down_diff(up, down):
+    return up - down
+
+
+def score_average_rating(up, down):
+    if up + down == 0:
+        return 0
+    return up / (up + down)
+
+#####################
+# score_pos_neg_diff
+#####################
+
+df.head(20)
+df["score_pos_neg_diff"] = df.apply(lambda x: score_up_down_diff(x["helpful_yes"], x["helpful_no"]), axis=1)
+
+df.sort_values("score_pos_neg_diff", ascending=False).head(20)
+
+
+# 500 0 -> 500 (daha iyi bir yorum pozitiflik açısından)
+# 1500 1000 -> 500
+
+
+
+# score_average_rating
+df["score_average_rating"] = df.apply(lambda x: score_average_rating(x["helpful_yes"], x["helpful_no"]), axis=1)
+
+df.sort_values("score_average_rating", ascending=False).head(20)
+
+# 500 0 -> 500    500 / 500 1
+# 1 0 -> 1/1 1
+
+
+# wilson_lower_bound
+df["wilson_lower_bound"] = df.apply(lambda x: wilson_lower_bound(x["helpful_yes"], x["helpful_no"]), axis=1)
+
+df.sort_values("wilson_lower_bound", ascending=False).head(20)
+
+
+
+##################################################
+# Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
+###################################################
+
+df.sort_values("wilson_lower_bound", ascending=False).head(20)
+
+
+
+
+##################################################
+# BONUS
+##################################################
+
+# wilson lower bound değerine göre değerlendirmeleri sıraladık. Helpful olarak işaretlenme sayısının yüksek olması
+# yapılan yorumun iyi olduğunu göstermez. Yorumları belirlerken hangi yorumların olumlu, olumsuz ve nötr olduğunu da görmek
+# bizlere fayda sağlayacaktır. Bu nedenle yapılan yorumlara sentiment analizi yani duygu analizi yaparak positive, negatif, nötr
+# sınıflandırması yapabiliriz. Bunun için Natural Language Toolkit kütüphanesi kullanacağız.
+
+# NLTK -> Doğal dil işleme kütüphanesi (https://www.nltk.org/)
+# Sentiment analysis (Duygu analizi), metindeki olumlu veya olumsuz duyguyu tespit etme sürecidir.
+# Genellikle şirketler veya markalar tarafından sosyal verilerdeki duyarlılığı tespit etmek, marka itibarını ölçmek ve müşterileri anlamak için kullanılır.
+
+
+# Gerekli kütüphanelerin yüklenmesi
+from nltk.sentiment.vader import SentimentIntensityAnalyzer #nltk kütüphanesinde hazır bir sentiment Intensity Analizi var
+import nltk
+import re
+from textblob import TextBlob # metnin olumlu-olumsuz durumuna göre size 0-1 aralığında bir değer dönmektedir. TextBlob ile amacımız yazının olumlu mu olumsuz mu içerik içerdiğini anlamaktır.
+nltk.downloader.download('vader_lexicon')
+# VADER (Valence Aware Dictionary for Sentiment Reasoning), duygunun hem polaritesine (pozitif/negatif) hem de
+# yoğunluğuna (gücüne) duyarlı olan metin duygu analizi için kullanılan bir modeldir.
+# NLTK paketinde mevcuttur ve doğrudan etiketlenmemiş metin verilerine uygulanabilir.
+
+
+# Daha iyi sonuçlar için summary değişkenimizin içerisindeki text'leri temizlenmesi gerekiyor. (Noktalama işaretlerinden kurtulmak, küçük harfe çevirmek..)
+rt = lambda x: re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",x)
+df["summary"] = df["summary"].map(rt)
+df["summary"] = df["summary"].str.lower()
+df.head(10)
+
+
+# Sentiment analysis
+# TextBlob Çıktı olarak polarity ve subjectivity değerlerini döndürecektir.
+# Polarity  duygu durumunu yani olumlu mu olumsuz mu olduğunu belirtir.
+# Bize 0 ile 1 arasında değer döner. 1' e ne kadar yakınsa o kadar olumlu, 0'a ne kadar yakınsa o kadar olumsuzdur.
+df[['polarity', 'subjectivity']] = df['summary'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
+for index, row in df['summary'].iteritems():
+    print(index,row)
+    score = SentimentIntensityAnalyzer().polarity_scores(row)
+    neg = score['neg']
+    neu = score['neu']
+    pos = score['pos']
+    if neg > pos:
+        df.loc[index, 'sentiment'] = "negative"
+    elif pos > neg:
+        df.loc[index, 'sentiment'] = "positive"
+    else:
+        df.loc[index, 'sentiment'] = "neutral"
+    df.loc[index, 'neg'] = neg
+    df.loc[index, 'neu'] = neu
+    df.loc[index, 'pos'] = pos
+
+df.head(10)
+
+
+# Duygu analizlerinin dağılımı
+def count_values_in_column(data,feature):
+    total=data.loc[:,feature].value_counts(dropna=False)
+    percentage=round(data.loc[:,feature].value_counts(dropna=False,normalize=True)*100,2)
+    return pd.concat([total,percentage],axis=1,keys=['Total','Percentage'])
+
+count_values_in_column(df,"sentiment")
+#           Total  Percentage
+# positive   2691    54.75000
+# neutral    1856    37.76000
+# negative    368     7.49000
+
+
+# Worldcloud -> Daha fazla görünen kelimelere daha fazla önem vererek oluşturulan görsel
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+def create_wordcloud(text):
+    stopwords = set(STOPWORDS)
+    wc = WordCloud(background_color="white",
+                  max_words=3000,
+                  stopwords=stopwords,
+                  repeat=True)
+    wc.generate(str(text))
+    plt.figure()
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
+# Pozitif yorumlar için kelime bulutu
+create_wordcloud(df[df["sentiment"]=="positive"]["summary"].values)
+
+# Negatif yorumlar için kelime bulutu
+create_wordcloud(df[df["sentiment"]=="negative"]["summary"].values)
+
+# Nötr yorumlar için kelime bulutu
+create_wordcloud(df[df["sentiment"]=="neutral"]["summary"].values)
+
+
+# 20 Yorumu Belirlerken, artık yorumların olumlu, olumsuz ve notr olma durumlarını da katabiliriz.
+df[df["sentiment"]=="positive"].sort_values("wilson_lower_bound", ascending=False).head(20)
+
+
+
